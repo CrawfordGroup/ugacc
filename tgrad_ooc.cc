70ac24312168c23b836df768da108c161c40717d
T. Daniel Crawford <crawdad@vt.edu>
2014-01-27 17:21:08 -0500

    Corrected out-of-core density code.

--- tgrad_ooc.cc
+++ tgrad_ooc.cc
@@ -26,24 +26,6 @@
   batches for a given combination of OOO indices.
 
   -TDC, 1/2014
-
-  NB: Permutations of the T3 amplitudes in certains are permissible because
-  the densities are ultimately contracted against symmetric quantities.  For
-  example, a derivation of the T3 contribution to the OOOV two-electron
-  density yields:
-
-  G(jila) <-- -T*(lk,bc) (2 T(ijk,abc) - T(ijk,bac) - T(ijk,cba)
-
-  However, in the code below the second triples amplitude is written as
-  T(ijk,acb) for consistency with other terms with similar structure even
-  though that choice of index ordering actually should contribute to the
-  G(ijla) component of the density.  This still yields a correct OOOV
-  contribution to the two-electron energy because the density is ultimately
-  contracted with two-electron (derivative) integrals as:
-
-  E_OOOV += <ij|ka> G(ijka)
-
-
 */
 
 #include <string>
@@ -100,14 +82,14 @@ void tgrad_ooc(void)
               for(int l=0; l < no; l++) {
                 X2[i][l][a][b] -= (2.0*t3[a][b][c] - t3[a][c][b] - t3[c][b][a]) * ints[j][k][l][c+no];
                 Z2[i][l][a][b] -= l3[a][b][c] * ints[j][k][l][c+no];
-                moinfo.Gooov[j][i][l][a] -= (2.0*t3[a][b][c] - t3[a][c][b] - t3[c][b][a]) * t2s[l][k][b][c] + l3[a][b][c] * t2[l][k][b][c];
+                moinfo.Gooov[j][i][l][a] -= (2.0*t3[a][b][c] - t3[b][a][c] - t3[c][b][a]) * t2s[l][k][b][c] + l3[a][b][c] * t2[l][k][b][c];
               }
 
               for(int d=0; d < nv; d++) {
                 X2[i][j][a][d] += (2.0*t3[a][b][c] - t3[a][c][b] - t3[c][b][a]) * ints[d+no][k][b+no][c+no];
                 Z2[i][j][a][d] += l3[a][b][c] * ints[d+no][k][b+no][c+no];
                 moinfo.Dvv[a][b] += 0.5 * t3[b][c][d] * l3[a][c][d];
-                moinfo.Gvvvo[a][b][d][j] += (2.0*t3[a][b][c] - t3[a][c][b] - t3[c][b][a]) * t2s[k][i][c][d] + l3[a][b][c] * t2[k][i][c][d];
+                moinfo.Gvvvo[a][b][d][j] += (2.0*t3[a][b][c] - t3[b][a][c] - t3[a][c][b]) * t2s[k][i][c][d] + l3[a][b][c] * t2[k][i][c][d];
               }
 
             } // abc
